83ab561d02ad847a803dad894b7fd023bda7663f
Jeremy Schlossman <jeremyschlossman@gmail.com>
2023-02-06 14:36:41 -0600

    updated up to fight function

--- src/script.js
+++ src/script.js
@@ -2,13 +2,32 @@
 
 // TODO - write your code here.
 
-const randomDamage = () => Math.floor(Math.random() * 10 +1);
+const randomDamage = () => Math.floor(Math.random() * 10) +1;
 
 const chooseOption = (opt1, opt2) => {
-    let randNum = Math.random() < 0.5 ? 0 : 1;
-    return randNum = 0 ? opt1 : opt2;
+    const randNum = Math.random() < 0.5 ? 0 : 1 ;
+    return randNum === 0 ? opt1 : opt2;
 }
 
 const attackPlayer = function(health) {
-    return health - randomDamage;
+    return parsInt(health, 10) - randomDamage; 
+}
+
+const logHealth = (player, health) => console.log(`${player} health: ${health}.`)
+
+const logDeath = (winner, loser) => console.log(`${winner} defeated ${loser}.`)
+
+const isDead = (health) => health <= 0;
+
+//fight
+
+function fight(player1, player2, player1Health, player2Health) {
+    while (true) {
+        const attacker = chooseOption(player1, player2)
+        if (attacker === player1) {
+            let player2Health = attackPlayer
+
+
+    }
+}
 }
