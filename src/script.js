658a099c06d8f8dc093bb09e2fc64b40e68c7e55
Jeremy Schlossman <jeremyschlossman@gmail.com>
2023-02-06 16:12:25 -0600

    finished fight function

--- src/script.js
+++ src/script.js
@@ -10,24 +10,38 @@ const chooseOption = (opt1, opt2) => {
 }
 
 const attackPlayer = function(health) {
-    return parsInt(health, 10) - randomDamage; 
+    return health - randomDamage; 
 }
 
-const logHealth = (player, health) => console.log(`${player} health: ${health}.`)
+const logHealth = (player, health) => console.log(`${player} health: ${health}`);
 
-const logDeath = (winner, loser) => console.log(`${winner} defeated ${loser}.`)
+const logDeath = (winner, loser) => console.log(`${winner} defeated ${loser}`);
 
 const isDead = (health) => health <= 0;
 
 //fight
 
 function fight(player1, player2, player1Health, player2Health) {
+
     while (true) {
-        const attacker = chooseOption(player1, player2)
+
+        const attacker = chooseOption(player1, player2);
+
         if (attacker === player1) {
-            let player2Health = attackPlayer
-
-
-    }
+            const player2Health = attackPlayer(player2Health);
+            logHealth(player2, player2Health);
+            if (isDead(player2Health === true)) {
+                logDeath(player1, player2);
+                break;
+            }
+        } else {
+            const player1Health = attackPlayer(player1Health)
+            if (isDead(player1Health) === true) {
+                logDeath(player2, player1);
+                break;
+            }
+        }
 }
 }
+
+fight("Joker", "Akechi", 100, 100);
