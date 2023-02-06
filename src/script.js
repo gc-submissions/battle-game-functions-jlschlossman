a835d19f40138f89abfce103ec25857ba58b706a
Jeremy Schlossman <jeremyschlossman@gmail.com>
2023-02-06 13:07:45 -0600

    first update to script.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,14 @@
 "use strict";
 
 // TODO - write your code here.
+
+const randomDamage = () => Math.floor(Math.random() * 10 +1);
+
+const chooseOption = (opt1, opt2) => {
+    let randNum = Math.random() < 0.5 ? 0 : 1;
+    return randNum = 0 ? opt1 : opt2;
+}
+
+const attackPlayer = function(health) {
+    return health - randomDamage;
+}
